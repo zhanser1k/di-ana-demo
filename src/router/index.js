4f3aa22661d7655ffee9a2d39ed0052c861b118d
zhanser1k <z.baykadir@amarkets.com>
2024-07-28 14:58:57 +0500

    chore: redirects

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'root',
+      redirect: HomeView
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
